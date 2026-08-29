05c416ab81920880b12aa89796396ba57cb74a8a
Ingridnery <neryingrid16@gmail.com>
2026-08-29 08:15:50 -0300

    Adicionando concorrencia com lock e implementando validacao de integridade do book

--- MatchingEngine.Core/Domain/Engine/MatchingEngineImpl.cs
+++ MatchingEngine.Core/Domain/Engine/MatchingEngineImpl.cs
@@ -5,9 +5,13 @@ namespace MatchingEngine.Core.Engine;
 
 public class MatchingEngineImpl : IMatchingEngine
 {
+    private readonly Lock bookLock = new ();
     private static IComparer<decimal> buyPriceComparer = Comparer<decimal>.Create((a,b) => b.CompareTo(a));
     private SortedDictionary<decimal, Queue<Order>> buyOrders = new SortedDictionary<decimal, Queue<Order>>(buyPriceComparer);
     private SortedDictionary<decimal, Queue<Order>> sellOrders = new SortedDictionary<decimal, Queue<Order>>();
+    private long totalBuyReceived;
+    private long totalSellReceived;
+    private long totalExecuted;
 
     private void AddOrderToBook(SortedDictionary<decimal, Queue<Order>> book, Order order)
     {
@@ -24,120 +28,129 @@ public class MatchingEngineImpl : IMatchingEngine
     }
     public List<Trade> ProcessOrder(Order order)
     {
-        int quantidadeTrade;
-
-        if(order.Side == Side.Buy)
-        {
-            var matchingSellOrders = sellOrders.FirstOrDefault().Value;
-
-            if(matchingSellOrders == null || matchingSellOrders.Count == 0 || matchingSellOrders.Peek().Price > order.Price)
+        int quantityTrade;
+        lock (bookLock)
+        {     
+            if(order.Side == Side.Buy)
             {
-                AddOrderToBook(buyOrders, order);
-                return new List<Trade>();
-            }
-            else
-            {
-                var sellOrder = matchingSellOrders.Peek();
-                
-                if(order.Quantity > sellOrder.Quantity)
+                totalBuyReceived += order.Quantity;
+                var matchingSellOrders = sellOrders.FirstOrDefault().Value;
+
+                if(matchingSellOrders == null || matchingSellOrders.Count == 0 || matchingSellOrders.Peek().Price > order.Price)
                 {
-                    order.Quantity -= sellOrder.Quantity;
-                    matchingSellOrders.Dequeue();
-                    if(matchingSellOrders.Count == 0)
-                    {
-                        sellOrders.Remove(sellOrder.Price);
-                    }
                     AddOrderToBook(buyOrders, order);
-                    quantidadeTrade = sellOrder.Quantity;
-                }
-                else if(order.Quantity < sellOrder.Quantity)
-                {
-                    sellOrder.Quantity -= order.Quantity;
-                    quantidadeTrade = order.Quantity;
-                    order.Quantity = 0;
+                    return new List<Trade>();
                 }
                 else
                 {
-                    matchingSellOrders.Dequeue();
-                    if(matchingSellOrders.Count == 0)
+                    var sellOrder = matchingSellOrders.Peek();
+                    
+                    if(order.Quantity > sellOrder.Quantity)
                     {
-                        sellOrders.Remove(sellOrder.Price);
+                        order.Quantity -= sellOrder.Quantity;
+                        matchingSellOrders.Dequeue();
+                        if(matchingSellOrders.Count == 0)
+                        {
+                            sellOrders.Remove(sellOrder.Price);
+                        }
+                        AddOrderToBook(buyOrders, order);
+                        quantityTrade = sellOrder.Quantity;
                     }
-                    quantidadeTrade = order.Quantity;
-                    order.Quantity = 0;
-                }
+                    else if(order.Quantity < sellOrder.Quantity)
+                    {
+                        sellOrder.Quantity -= order.Quantity;
+                        quantityTrade = order.Quantity;
+                        order.Quantity = 0;
+                    }
+                    else
+                    {
+                        matchingSellOrders.Dequeue();
+                        if(matchingSellOrders.Count == 0)
+                        {
+                            sellOrders.Remove(sellOrder.Price);
+                        }
+                        quantityTrade = order.Quantity;
+                        order.Quantity = 0;
+                    }
 
-                var trade = new Trade
-                {
-                    Id = Guid.NewGuid(),
-                    MakerOrderId = sellOrder.Id,
-                    TakerOrderId = order.Id,
-                    Price = sellOrder.Price,
-                    Quantity = quantidadeTrade,
-                };
-
-                return new List<Trade> { trade };
-            } 
-        }
-        else
-        {
-            var matchingBuyOrders = buyOrders.FirstOrDefault().Value;
-
-            if(matchingBuyOrders == null || matchingBuyOrders.Count == 0 || matchingBuyOrders.Peek().Price < order.Price)
-            {
-                AddOrderToBook(sellOrders, order);
-                return new List<Trade>();
+                    var trade = new Trade
+                    {
+                        Id = Guid.NewGuid(),
+                        MakerOrderId = sellOrder.Id,
+                        TakerOrderId = order.Id,
+                        Price = sellOrder.Price,
+                        Quantity = quantityTrade,
+                    };
+                    totalExecuted += quantityTrade;
+                    return new List<Trade> { trade };
+                } 
             }
             else
             {
-                var buyOrder = matchingBuyOrders.Peek();
-                
-                if(order.Quantity > buyOrder.Quantity)
+                totalSellReceived += order.Quantity;
+                var matchingBuyOrders = buyOrders.FirstOrDefault().Value;
+
+                if(matchingBuyOrders == null || matchingBuyOrders.Count == 0 || matchingBuyOrders.Peek().Price < order.Price)
                 {
-                    order.Quantity -= buyOrder.Quantity;
-                    matchingBuyOrders.Dequeue();
-                    if(matchingBuyOrders.Count == 0)
-                    {
-                        buyOrders.Remove(buyOrder.Price);
-                    }
                     AddOrderToBook(sellOrders, order);
-                    quantidadeTrade = buyOrder.Quantity;
-                }
-                else if(order.Quantity < buyOrder.Quantity)
-                {
-                    buyOrder.Quantity -= order.Quantity;
-                    quantidadeTrade = order.Quantity;
-                    order.Quantity = 0;
+                    return new List<Trade>();
                 }
                 else
                 {
-                    matchingBuyOrders.Dequeue();
-                    if(matchingBuyOrders.Count == 0)
+                    var buyOrder = matchingBuyOrders.Peek();
+                    
+                    if(order.Quantity > buyOrder.Quantity)
                     {
-                        buyOrders.Remove(buyOrder.Price);
+                        order.Quantity -= buyOrder.Quantity;
+                        matchingBuyOrders.Dequeue();
+                        if(matchingBuyOrders.Count == 0)
+                        {
+                            buyOrders.Remove(buyOrder.Price);
+                        }
+                        AddOrderToBook(sellOrders, order);
+                        quantityTrade = buyOrder.Quantity;
                     }
-                    quantidadeTrade = order.Quantity;
-                    order.Quantity = 0;
+                    else if(order.Quantity < buyOrder.Quantity)
+                    {
+                        buyOrder.Quantity -= order.Quantity;
+                        quantityTrade = order.Quantity;
+                        order.Quantity = 0;
+                    }
+                    else
+                    {
+                        matchingBuyOrders.Dequeue();
+                        if(matchingBuyOrders.Count == 0)
+                        {
+                            buyOrders.Remove(buyOrder.Price);
+                        }
+                        quantityTrade = order.Quantity;
+                        order.Quantity = 0;
+                    }
+
+                    var trade = new Trade
+                    {
+                        Id = Guid.NewGuid(),
+                        MakerOrderId = buyOrder.Id,
+                        TakerOrderId = order.Id,
+                        Price = buyOrder.Price,
+                        Quantity = quantityTrade,
+                    };
+                    totalExecuted += quantityTrade;
+
+                    return new List<Trade> { trade };
                 }
-
-                var trade = new Trade
-                {
-                    Id = Guid.NewGuid(),
-                    MakerOrderId = buyOrder.Id,
-                    TakerOrderId = order.Id,
-                    Price = buyOrder.Price,
-                    Quantity = quantidadeTrade,
-                };
-
-                return new List<Trade> { trade };
             }
-        }
+        }    
     }
 
     // Usado no Chaos Test para validar que lotes comprados == lotes vendidos
     public bool ValidarIntegridadeDoBook()
     {
-        throw new NotImplementedException();
+        long totalBuyOrders = GetBuyOrders().Sum(o => o.Quantity);
+        long totalSellOrders = GetSellOrders().Sum(o => o.Quantity);
+        if(totalBuyOrders == (totalBuyReceived - totalExecuted) && totalSellOrders == (totalSellReceived - totalExecuted))
+            return true;
+        return false;
     }
 
     public List<Order> GetBuyOrders()
