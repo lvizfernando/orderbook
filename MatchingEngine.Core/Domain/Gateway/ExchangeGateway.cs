5c46ce872ad066048ef9514735b67bb855e6d28c
Ingrid Nery <ingrid.nery@luz-ef.com>
2026-09-08 14:38:52 -0300

    Melhorando performance

--- MatchingEngine.Core/Domain/Gateway/ExchangeGateway.cs
+++ MatchingEngine.Core/Domain/Gateway/ExchangeGateway.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using MatchingEngine.Core.Abstractions;
 using MatchingEngine.Core.Domain;
 
@@ -6,15 +7,42 @@ namespace MatchingEngine.Core.Gateway;
 public class ExchangeGateway
 {
     private readonly IMatchingEngine _engine;
+    private readonly BlockingCollection<(Order Order, TaskCompletionSource<List<Trade>> Completion)> _queue = new();
 
     public ExchangeGateway(IMatchingEngine engine)
     {
         _engine = engine;
+         var worker = new Thread(ConsumeLoop)
+        {
+            IsBackground = true,
+            Name = "MatchingEngine-Worker"
+        };
+        worker.Start();
+
     }
 
-    public async Task<List<Trade>> ReceiveOrderAsync(Order order)
+    public Task<List<Trade>> ReceiveOrderAsync(Order order)
     {
-        // TODO: chamada thread-safe para _engine.ProcessOrder(order)
-        return _engine.ProcessOrder(order);
+        var tcs = new TaskCompletionSource<List<Trade>>(TaskCreationOptions.RunContinuationsAsynchronously);
+        _queue.Add((order, tcs));
+        return tcs.Task;
+
     }
+
+     private void ConsumeLoop()
+    {
+        foreach (var (order, tcs) in _queue.GetConsumingEnumerable())
+        {
+            try
+            {
+                var trades = _engine.ProcessOrder(order);
+                tcs.SetResult(trades);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        }
+    }
+
 }
